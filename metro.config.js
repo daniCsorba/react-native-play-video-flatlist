4894e11d6d4e54aaeca18cd3263516f1836972dd
daniCsorba <98473548+daniCsorba@users.noreply.github.com>
2022-03-10 11:43:58 +0100

    updated

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,16 @@
  *
  * @format
  */
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
+// exclusionList is a function that takes an array of regexes and combines
+// them with the default exclusions to return a single regex.
+  
+  
 module.exports = {
+  resolver: {
+    blacklistRE: exclusionList([/dist\/.*/])
+    }
   transformer: {
     getTransformOptions: async () => ({
       transform: {
